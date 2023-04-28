5ac039666a0209ae334e85bf9cf1636557484526
SoulMelody <lovestar@buaa.edu.cn>
2023-04-28 13:00:56 +0800

    add more setting items

--- libresvip/gui/modules/config_items.py
+++ libresvip/gui/modules/config_items.py
@@ -1,21 +1,25 @@
-import os
+import pathlib
 
+from omegaconf import OmegaConf
 from qmlease import slot
 from qtpy.QtCore import QObject
+
+from libresvip.core.config import config_path, settings
 
 
 class ConfigItems(QObject):
     def __init__(self, parent=None):
         QObject.__init__(self, parent=parent)
-        self._save_folder = ""
 
     @slot(result=str)
     def get_save_folder(self):
-        return self._save_folder
+        return str(settings.save_folder)
 
     @slot(str, result=bool)
     def set_save_folder(self, value) -> bool:
-        if os.path.exists(value) and os.path.isdir(value):
-            self._save_folder = value
+        path = pathlib.Path(value)
+        if path.exists() and path.is_dir():
+            settings.save_folder = path
+            OmegaConf.save(settings, config_path)
             return True
         return False
