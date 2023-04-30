520b87a31c4e71a3c024fd0f388b89a54705d9d3
SoulMelody <lovestar@buaa.edu.cn>
2023-04-30 09:09:44 +0800

    add packaging scripts

---
+++ deploy/packaging/setup_gui.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+import pathlib
+import sys
+
+import PySide6
+from cx_Freeze import Executable, setup
+from cx_Freeze.hooks import _qthooks
+
+try:
+    from cx_Freeze.hooks import get_qt_plugins_paths
+except ImportError:
+    get_qt_plugins_paths = None
+
+
+for func in dir(_qthooks):
+    if func.startswith("load_qt_qtweb"):
+        setattr(_qthooks, func, lambda _, __: None)
+
+pyside6_dir = pathlib.Path(PySide6.__file__).parent
+include_files = [
+    (pyside6_dir / "qml", pathlib.Path("./lib/PySide6/qml")),
+]
+if get_qt_plugins_paths:
+    # Inclusion of extra plugins (since cx_Freeze 6.8b2)
+    # cx_Freeze imports automatically the following plugins depending of the
+    # use of some modules:
+    # imageformats, platforms, platformthemes, styles - QtGui
+    # mediaservice - QtMultimedia
+    # printsupport - QtPrintSupport
+    for plugin_name in (
+        # "accessible",
+        # "iconengines",
+        # "platforminputcontexts",
+        # "xcbglintegrations",
+        # "egldeviceintegrations",
+        "wayland-decoration-client",
+        "wayland-graphics-integration-client",
+        # "wayland-graphics-integration-server",
+        "wayland-shell-integration",
+    ):
+        include_files += get_qt_plugins_paths("PySide6", plugin_name)
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+build_exe_options = {
+    "bin_excludes": [],
+    # exclude packages that are not really needed
+    "excludes": [
+        "tkinter",
+        "unittest",
+        "pydoc",
+        "pydoc_data",
+        "black",
+        "jedi",
+        "numpy",
+        "IPython",
+        "Cython",
+        "sqlite3",
+        "test",
+        "PIL",
+        "trame",
+        "trame_client",
+        "trame_router",
+        "trame_server",
+        "wslink",
+        "webview",
+        "aiohttp",
+    ],
+    "include_files": include_files,
+    "zip_include_packages": ["PySide6"],
+    "packages": ["qmlease", "libresvip", "PySide6.QtQuick"],
+}
+
+executables = [
+    Executable(
+        "../../libresvip/gui/__main__.py", base=base, target_name="libresvip.gui"
+    ),
+]
+
+setup(
+    name="LibreSVIP",
+    version="0.1",
+    options={"build_exe": build_exe_options},
+    executables=executables,
+)
