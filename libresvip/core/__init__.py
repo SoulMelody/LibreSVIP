f16dd78a53dc4766725786298cd943f0f604a4ac
SoulMelody <lovestar@buaa.edu.cn>
2023-05-31 04:50:09 +0800

    patch converter exe

--- libresvip/core/__init__.py
+++ libresvip/core/__init__.py
@@ -1,9 +1,7 @@
 import contextlib
 
 with contextlib.suppress(ImportError, RuntimeError):
-    import os
-    import sys
-
     from imageio_ffmpeg import get_ffmpeg_exe
+    from pydub import AudioSegment
 
-    sys.path.append(os.path.dirname(get_ffmpeg_exe()))
+    AudioSegment.converter = get_ffmpeg_exe()
