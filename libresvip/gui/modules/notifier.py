ae2f49750c477341cf3432663db16c814bb18682
SoulMelody <lovestar@buaa.edu.cn>
2023-06-02 07:27:24 +0800

    switch to notify-py when using pypy

--- libresvip/gui/modules/notifier.py
+++ libresvip/gui/modules/notifier.py
@@ -1,4 +1,4 @@
-from desktop_notifier import DesktopNotifier
+from loguru import logger
 from qmlease import slot
 from qtpy.QtCore import QObject
 
@@ -9,11 +9,30 @@ class Notifier(QObject):
     def __init__(self):
         super().__init__()
         try:
-            self.notifier = DesktopNotifier(PACKAGE_NAME, res_dir / "libresvip.ico")
+            from desktop_notifier import DesktopNotifier
+
+            self.notifier = DesktopNotifier(
+                app_name=PACKAGE_NAME, app_icon=res_dir / "libresvip.ico"
+            )
+            self.notification = None
         except Exception:
+            from notifypy import Notify
+
+            self.notification = Notify(
+                default_notification_application_name=PACKAGE_NAME,
+                default_notification_icon=res_dir / "libresvip.ico",
+            )
             self.notifier = None
 
     @slot(str, str)
     def notify(self, title, message):
-        if self.notifier:
-            self.notifier.send_sync(title=title, message=message)
+        try:
+            if self.notifier:
+                self.notifier.send(title=title, message=message)
+            elif self.notification:
+                self.notification.title = title
+                self.notification.message = message
+
+                self.notification.send()
+        except Exception as e:
+            logger.error(e)
