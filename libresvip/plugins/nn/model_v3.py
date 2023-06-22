0c1c2dad71bb3b185adc791e5efd0227e6eb200d
SoulMelody <yjxrtzyx@gmail.com>
2023-06-22 21:53:52 +0800

    add schemas for niaoniao v3 alpha

---
+++ libresvip/plugins/nn/model_v3.py
@@ -0,0 +1,46 @@
+from pydantic import Field
+
+from libresvip.model.base import BaseModel
+
+
+class NoteDictListItem(BaseModel):
+    vib_rat: int = Field(alias='VIB_rat')
+    vib_len: int = Field(alias='VIB_len')
+    start_64note: int
+    pbs: int = Field(alias='PBS')
+    cle: float = Field(alias='CLE')
+    vib_dep: int = Field(alias='VIB_dep')
+    lyric: str
+    len_64note: int
+    vel: float = Field(alias='VEL')
+    dyn_list: list[int] = Field(alias='DYN_list')
+    id: int
+    row: int
+    is_triple: bool
+    pinyin: str
+    pit_list: list[int] = Field(alias='PIT_list')
+
+
+class TrackDictListItem(BaseModel):
+    character_path: str
+    note_dict_list: list[NoteDictListItem]
+    is_mute: bool
+    volume: float
+    inf_path: str
+    is_solo: bool
+    num_note: int
+    index: int
+    voice_path: str
+    readme_path: str
+    name: str
+    head_path: str
+    type: int
+
+
+class Niao3Project(BaseModel):
+    beat_per_bar: int
+    bpm_float: float
+    num_bar: int
+    track_dict_list: list[TrackDictListItem]
+    num_track: int
+    base_beat: int
