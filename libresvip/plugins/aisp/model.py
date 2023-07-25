c2a6b1d0e64651d061c2aadfd74037a9c2d32fe7
SoulMelody <yjxrtzyx@gmail.com>
2023-07-26 07:56:19 +0800

    update aisp model

--- libresvip/plugins/aisp/model.py
+++ libresvip/plugins/aisp/model.py
@@ -1,6 +1,13 @@
-from typing import Optional
+from typing import Optional, Union
 
-from pydantic import Field
+from pydantic import (
+    Field,
+    SerializationInfo,
+    ValidationInfo,
+    computed_field,
+    field_serializer,
+    field_validator,
+)
 
 from libresvip.model.base import BaseModel
 
@@ -13,7 +20,55 @@ class AISNote(BaseModel):
     pinyin: Optional[str] = Field(alias="py")
     vel: Optional[int] = None
     tri: Optional[bool] = None
-    pit: Optional[str] = None
+    pit: Optional[list[float]] = None
+
+    @field_validator("pit", mode="before")
+    @classmethod
+    def validate_pit(
+        cls, value: Union[str, list[Union[float, str]]], _info: ValidationInfo
+    ) -> Optional[list[float]]:
+        if value is None:
+            return None
+        if isinstance(value, str):
+            value = value.split()
+        pit_list = []
+        for x in value:
+            if isinstance(x, str) and "0x" in x:
+                pit_list.extend([0] * int(x[2:]))
+            elif isinstance(x, str):
+                pit_list.append(float(x))
+            else:
+                pit_list.append(x)
+        return pit_list
+
+    @field_serializer("pit", when_used="json-unless-none")
+    @classmethod
+    def serialize_pit(
+        cls, value: Optional[list[float]], _info: SerializationInfo
+    ) -> str:
+        if value is None:
+            return ""
+        pit_str = ""
+        i = 0
+        s0 = None
+        e0 = None
+        while i < len(value):
+            if s0 is None and value[i] == 0:
+                s0 = i
+                while (
+                    e0 is None
+                    and value[i] == 0
+                    and (value[i + 1] != 0 or i + 1 >= len(value))
+                ):
+                    e0 = i
+                    pit_str += "0x%d " % ((e0 - s0) + 1) if e0 > s0 else "0 "
+                    i += 1
+                    s0 = None
+                    e0 = None
+            if value[i] != 0:
+                pit_str += f"{round(value[i], 2)} "
+            i += 1
+        return pit_str.strip()
 
 
 class AISPattern(BaseModel):
@@ -37,10 +92,13 @@ class AISTrack(BaseModel):
 
 
 class AISTimeSignature(BaseModel):
-    str_value: Optional[str] = Field(alias="str")
     beat_zi: Optional[int] = None
     beat_mu: Optional[int] = None
     start_bar: Optional[int] = None
+
+    @computed_field(alias="str")
+    def str_value(self) -> str:
+        return f"{self.beat_zi}/{self.beat_mu}"
 
 
 class AISTempo(BaseModel):
