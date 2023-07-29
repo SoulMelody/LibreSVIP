b7673a09af27bc1345ce31918e6a8269fb339e14
SoulMelody <yjxrtzyx@gmail.com>
2023-07-29 17:12:43 +0800

    ccs: support instrumental track conversion

--- libresvip/plugins/ccs/cevio_generator.py
+++ libresvip/plugins/ccs/cevio_generator.py
@@ -1,14 +1,17 @@
+import contextlib
 import dataclasses
 import datetime
-import math
 import uuid
 from typing import Optional
 
+from pydub import AudioSegment
+from pydub.exceptions import CouldntDecodeError
 from xsdata.models.datatype import XmlTime
 
 from libresvip.core.constants import KEY_IN_OCTAVE
 from libresvip.core.time_sync import TimeSynchronizer
 from libresvip.model.base import (
+    InstrumentalTrack,
     Note,
     ParamCurve,
     Project,
@@ -55,50 +58,68 @@ class CeVIOGenerator:
         )
         scene_node = ccs_project.sequence.scene
 
-        self.first_bar_length = int(project.time_signature_list[0].bar_length() * TICK_RATE)
+        self.first_bar_length = int(
+            project.time_signature_list[0].bar_length() * TICK_RATE
+        )
 
         default_tempos = self.generate_tempos(project.song_tempo_list)
         default_beats = self.generate_time_signatures(project.time_signature_list)
 
-        for new_unit, new_group in self.generate_tracks(project.track_list, default_tempos, default_beats, project.song_tempo_list):
+        for new_unit, new_group in self.generate_tracks(
+            project.track_list, default_tempos, default_beats, project.song_tempo_list
+        ):
             scene_node.units.unit.append(new_unit)
             scene_node.groups.group.append(new_group)
 
         return ccs_project
 
     def generate_tempos(self, tempos: list[SongTempo]) -> CeVIOTempo:
-        tempo_node = CeVIOTempo(
-            sound=[CeVIOSound(clock=0, tempo=tempos[0].bpm)]
-        )
+        tempo_node = CeVIOTempo(sound=[CeVIOSound(clock=0, tempo=tempos[0].bpm)])
         for model in tempos[1:]:
-            tempo_node.sound.append(CeVIOSound(
-                clock=int(model.position * TICK_RATE) + self.first_bar_length,
-                tempo=model.bpm,
-            ))
+            tempo_node.sound.append(
+                CeVIOSound(
+                    clock=int(model.position * TICK_RATE) + self.first_bar_length,
+                    tempo=model.bpm,
+                )
+            )
         return tempo_node
 
-    def generate_time_signatures(self, time_signatures: list[TimeSignature]) -> CeVIOBeat:
+    def generate_time_signatures(
+        self, time_signatures: list[TimeSignature]
+    ) -> CeVIOBeat:
         beat = CeVIOBeat(
-            time=[CeVIOTime(
-                clock=0,
-                beats=time_signatures[0].numerator,
-                beat_type=time_signatures[0].denominator,
-            )]
+            time=[
+                CeVIOTime(
+                    clock=0,
+                    beats=time_signatures[0].numerator,
+                    beat_type=time_signatures[0].denominator,
+                )
+            ]
         )
         tick = 0.0
         prev_time_signature = time_signatures[0]
         for time_signature in time_signatures[1:]:
             if time_signature.bar_index > prev_time_signature.bar_index:
-                tick += (time_signature.bar_index - prev_time_signature.bar_index) * prev_time_signature.bar_length()
-            beat.time.append(CeVIOTime(
-                clock=int(tick * TICK_RATE) + self.first_bar_length,
-                beats=time_signature.numerator,
-                beat_type=time_signature.denominator,
-            ))
+                tick += (
+                    time_signature.bar_index - prev_time_signature.bar_index
+                ) * prev_time_signature.bar_length()
+            beat.time.append(
+                CeVIOTime(
+                    clock=int(tick * TICK_RATE) + self.first_bar_length,
+                    beats=time_signature.numerator,
+                    beat_type=time_signature.denominator,
+                )
+            )
             prev_time_signature = time_signature
         return beat
 
-    def generate_tracks(self, tracks: list[Track], tempo: CeVIOTempo, beat: CeVIOBeat, tempo_list: list[SongTempo]) -> list[tuple[CeVIOUnit, CeVIOGroup]]:
+    def generate_tracks(
+        self,
+        tracks: list[Track],
+        tempo: CeVIOTempo,
+        beat: CeVIOBeat,
+        tempo_list: list[SongTempo],
+    ) -> list[tuple[CeVIOUnit, CeVIOGroup]]:
         results = []
         for track in tracks:
             if isinstance(track, SingingTrack):
@@ -107,44 +128,85 @@ class CeVIOGenerator:
                     name=track.title,
                     is_muted=track.mute,
                     is_solo=track.solo,
-                    cast_id=self.options.default_singer_id
+                    cast_id=self.options.default_singer_id,
                 )
                 new_unit = CeVIOUnit(
-                    cast_id=self.options.default_singer_id, group=new_group.group_id,
+                    cast_id=self.options.default_singer_id,
+                    group=new_group.group_id,
                     start_time=XmlTime.from_string("00:00:00.000"),
-                    song=CeVIOSong(
-                        tempo=tempo, beat=beat
-                    )
+                    song=CeVIOSong(tempo=tempo, beat=beat),
                 )
                 new_unit.song.score.note = self.generate_notes(track.note_list)
                 if len(track.note_list):
                     max_tick = track.note_list[-1].end_pos
-                    max_secs = self.time_synchronizer.get_actual_secs_from_ticks(max_tick)
-                    new_unit.duration = XmlTime.from_time(datetime.time(second=math.ceil(max_secs)))
+                    max_secs = self.time_synchronizer.get_actual_secs_from_ticks(
+                        max_tick
+                    )
+                    new_unit.duration = XmlTime.from_time(
+                        datetime.datetime.utcfromtimestamp(max_secs).time()
+                    )
 
                 if log_f0 := self.generate_pitch(track.edited_params.pitch, tempo_list):
                     new_unit.song.parameter.log_f0 = log_f0
                 results.append((new_unit, new_group))
+            elif isinstance(track, InstrumentalTrack):
+                with contextlib.suppress(CouldntDecodeError, FileNotFoundError):
+                    audio_segment = AudioSegment.from_file(track.audio_file_path)
+                    start_secs = self.time_synchronizer.get_actual_secs_from_ticks(
+                        track.offset
+                    )
+                    start_time = XmlTime.from_time(
+                        datetime.datetime.utcfromtimestamp(start_secs).time()
+                    )
+                    end_time = XmlTime.from_time(
+                        datetime.datetime.utcfromtimestamp(
+                            audio_segment.duration_seconds
+                        ).time()
+                    )
+                    new_group = CeVIOGroup(
+                        group_id=str(uuid.uuid4()),
+                        name=track.title,
+                        is_muted=track.mute,
+                        is_solo=track.solo,
+                        category="OuterAudio",
+                    )
+                    new_unit = CeVIOUnit(
+                        category="OuterAudio",
+                        group=new_group.group_id,
+                        file_path=track.audio_file_path,
+                        start_time=start_time,
+                        duration=end_time,
+                    )
+                    results.append((new_unit, new_group))
         return results
 
     def generate_notes(self, notes: list[Note]) -> list[CeVIONote]:
         ccs_notes = []
         for note in notes:
-            ccs_notes.append(CeVIONote(
-                clock=int(note.start_pos * TICK_RATE) + self.first_bar_length,
-                duration=int(note.length * TICK_RATE),
-                lyric=note.lyric,
-                pitch_octave=note.key_number // KEY_IN_OCTAVE + OCTAVE_OFFSET,
-                pitch_step=note.key_number % KEY_IN_OCTAVE,
-            ))
+            ccs_notes.append(
+                CeVIONote(
+                    clock=int(note.start_pos * TICK_RATE) + self.first_bar_length,
+                    duration=int(note.length * TICK_RATE),
+                    lyric=note.lyric,
+                    pitch_octave=note.key_number // KEY_IN_OCTAVE + OCTAVE_OFFSET,
+                    pitch_step=note.key_number % KEY_IN_OCTAVE,
+                )
+            )
         return ccs_notes
 
-    def generate_pitch(self, pitch: ParamCurve, tempo_list: list[SongTempo]) -> Optional[CeVIOParameter]:
-        if (data := generate_for_cevio(pitch, tempo_list, self.first_bar_length)) is not None:
+    def generate_pitch(
+        self, pitch: ParamCurve, tempo_list: list[SongTempo]
+    ) -> Optional[CeVIOParameter]:
+        if (
+            data := generate_for_cevio(pitch, tempo_list, self.first_bar_length)
+        ) is not None:
             log_f0_node = CeVIOParameter(length=data.length)
-            log_f0_node.data.extend(CeVIOData(
-                index=each.index,
-                repeat=each.repeat,
-                value=each.value,
-            ) for each in data.events)
+            log_f0_node.data.extend(
+                CeVIOData(
+                    index=each.index,
+                    repeat=each.repeat,
+                    value=each.value,
+                )
+                for each in data.events
+            )
             return log_f0_node
--- libresvip/plugins/ccs/cevio_parser.py
+++ libresvip/plugins/ccs/cevio_parser.py
@@ -3,40 +3,72 @@ import itertools
 import operator
 from typing import Optional
 
-from libresvip.model.base import Note, Project, SingingTrack, SongTempo, TimeSignature, Track
+from libresvip.core.time_sync import TimeSynchronizer
+from libresvip.model.base import (
+    InstrumentalTrack,
+    Note,
+    Project,
+    SingingTrack,
+    SongTempo,
+    TimeSignature,
+    Track,
+)
 
 from .cevio_pitch import CeVIOPitchEvent, CeVIOTrackPitchData, pitch_from_cevio_track
 from .constants import OCTAVE_OFFSET, TICK_RATE
-from .model import CeVIOCreativeStudioProject, CeVIOData, CeVIOUnit
+from .model import CeVIOCreativeStudioProject, CeVIOData, CeVIOGroup, CeVIOUnit
 from .options import InputOptions
 
 
 @dataclasses.dataclass
 class CeVIOParser:
     options: InputOptions
+    time_synchronizer: TimeSynchronizer = dataclasses.field(init=False)
+    singer_id2name: dict[str, str] = dataclasses.field(default_factory=dict)
 
     def parse_project(self, ccs_project: CeVIOCreativeStudioProject) -> Project:
-
         scene_node = ccs_project.sequence.scene
-        unit_nodes = [unit_node for unit_node in scene_node.units.unit
-                    if unit_node.category == "SingerSong"]
-        group_nodes = [group_node for group_node in scene_node.groups.group
-                    if group_node.category == "SingerSong"]
+        for sound_source in ccs_project.generation.svss.sound_sources.sound_source:
+            self.singer_id2name[sound_source.sound_source_id] = sound_source.name
+        singing_unit_nodes = [
+            unit_node
+            for unit_node in scene_node.units.unit
+            if unit_node.category == "SingerSong"
+        ]
+        audio_unit_nodes = [
+            unit_node
+            for unit_node in scene_node.units.unit
+            if unit_node.category == "OuterAudio"
+        ]
+        id2group = {group.group_id: group for group in scene_node.groups.group}
 
         tracks: list[Track] = []
         tempos = []
         time_signatures = []
-        for index, unit_node in enumerate(unit_nodes):
+        for index, unit_node in enumerate(singing_unit_nodes):
             group_id = unit_node.group
-            group = next((group for group in group_nodes if group.group_id == group_id), None)
+            group = id2group.get(group_id, None)
             track_name = group.name if group is not None else None
-            track, tempo_part, time_signature_part = self.parse_track(index, unit_node, track_name)
+            track, tempo_part, time_signature_part = self.parse_singing_track(
+                index, unit_node, group, track_name
+            )
             tracks.append(track)
             tempos.extend(tempo_part)
             time_signatures.extend(time_signature_part)
 
         tempos = self.merge_tempos(sorted(tempos, key=operator.attrgetter("position")))
-        time_signatures = self.merge_time_signatures(sorted(time_signatures, key=operator.attrgetter("bar_index")))
+        time_signatures = self.merge_time_signatures(
+            sorted(time_signatures, key=operator.attrgetter("bar_index"))
+        )
+
+        self.time_synchronizer = TimeSynchronizer(tempos)
+
+        for index, unit_node in enumerate(audio_unit_nodes):
+            group_id = unit_node.group
+            group = id2group.get(group_id, None)
+            track_name = group.name if group is not None else None
+            track = self.parse_instrumental_track(index, unit_node, group, track_name)
+            tracks.append(track)
 
         return Project(
             track_list=tracks,
@@ -47,12 +79,12 @@ class CeVIOParser:
     def merge_tempos(self, tempos: list[SongTempo]) -> list[SongTempo]:
         return [
             next(group)
-            for key, group in itertools.groupby(
-                tempos, operator.attrgetter("position")
-            )
+            for key, group in itertools.groupby(tempos, operator.attrgetter("position"))
         ]
 
-    def merge_time_signatures(self, time_signatures: list[TimeSignature]) -> list[TimeSignature]:
+    def merge_time_signatures(
+        self, time_signatures: list[TimeSignature]
+    ) -> list[TimeSignature]:
         return [
             next(group)
             for key, group in itertools.groupby(
@@ -60,7 +92,32 @@ class CeVIOParser:
             )
         ]
 
-    def parse_track(self, index: int, unit_node: CeVIOUnit, track_name: Optional[str]) -> tuple[SingingTrack, list[SongTempo], list[TimeSignature]]:
+    def parse_instrumental_track(
+        self,
+        index: int,
+        unit_node: CeVIOUnit,
+        group_node: CeVIOGroup,
+        track_name: Optional[str],
+    ) -> InstrumentalTrack:
+        return InstrumentalTrack(
+            title=track_name or f"Track {index + 1}",
+            audio_file_path=unit_node.file_path,
+            mute=group_node.is_muted,
+            solo=group_node.is_solo,
+            offset=int(
+                self.time_synchronizer.get_actual_ticks_from_secs(
+                    unit_node.start_time.duration
+                )
+            ),
+        )
+
+    def parse_singing_track(
+        self,
+        index: int,
+        unit_node: CeVIOUnit,
+        group_node: CeVIOGroup,
+        track_name: Optional[str],
+    ) -> tuple[SingingTrack, list[SongTempo], list[TimeSignature]]:
         time_nodes = unit_node.song.beat.time
 
         prev_tick = 0
@@ -76,19 +133,34 @@ class CeVIOParser:
                 ticks_in_measure = time_signatures[-1].bar_length()
                 tick_diff = tick - prev_tick
                 measure_diff = tick_diff / ticks_in_measure
-                time_signatures.append(TimeSignature(bar_index=int(
-                    time_signatures[-1].bar_index + measure_diff,
-                ), numerator=numerator, denominator=denominator))
+                time_signatures.append(
+                    TimeSignature(
+                        bar_index=int(
+                            time_signatures[-1].bar_index + measure_diff,
+                        ),
+                        numerator=numerator,
+                        denominator=denominator,
+                    )
+                )
                 prev_tick = tick
 
-        time_signatures = [time_signature.model_copy(update={"bar_index": time_signature.bar_index - 4}) for time_signature in time_signatures]
+        time_signatures = [
+            time_signature.model_copy(
+                update={"bar_index": time_signature.bar_index - 4}
+            )
+            for time_signature in time_signatures
+        ]
 
         tick_prefix = int(time_signatures[0].bar_length())
 
         tempos = []
         tempo_nodes = unit_node.song.tempo.sound
         for tempo_node in tempo_nodes:
-            tick = tempo_node.clock // TICK_RATE - tick_prefix if tempo_node.clock is not None else None
+            tick = (
+                tempo_node.clock // TICK_RATE - tick_prefix
+                if tempo_node.clock is not None
+                else None
+            )
             bpm = float(tempo_node.tempo) if tempo_node.tempo is not None else None
             if tick is not None and bpm is not None:
                 tempos.append(SongTempo(position=tick, bpm=bpm))
@@ -96,27 +168,68 @@ class CeVIOParser:
         notes = []
         note_nodes = unit_node.song.score.note
         for note_node in note_nodes:
-            tick_on = (note_node.clock // TICK_RATE) - tick_prefix if note_node.clock is not None else None
-            duration = (note_node.duration // TICK_RATE) if note_node.duration is not None else None
-            pitch_step = note_node.pitch_step if note_node.pitch_step is not None else None
-            pitch_octave = note_node.pitch_octave - OCTAVE_OFFSET if note_node.pitch_octave is not None else None
-            key = pitch_step + pitch_octave * 12 if pitch_step is not None and pitch_octave is not None else None
+            tick_on = (
+                (note_node.clock // TICK_RATE) - tick_prefix
+                if note_node.clock is not None
+                else None
+            )
+            duration = (
+                (note_node.duration // TICK_RATE)
+                if note_node.duration is not None
+                else None
+            )
+            pitch_step = (
+                note_node.pitch_step if note_node.pitch_step is not None else None
+            )
+            pitch_octave = (
+                note_node.pitch_octave - OCTAVE_OFFSET
+                if note_node.pitch_octave is not None
+                else None
+            )
+            key = (
+                pitch_step + pitch_octave * 12
+                if pitch_step is not None and pitch_octave is not None
+                else None
+            )
             lyric = note_node.lyric or None
-            if tick_on is not None and duration is not None and key is not None and lyric is not None:
-                notes.append(Note(key_number=key, lyric=lyric, start_pos=tick_on, length=duration))
+            if (
+                tick_on is not None
+                and duration is not None
+                and key is not None
+                and lyric is not None
+            ):
+                notes.append(
+                    Note(
+                        key_number=key, lyric=lyric, start_pos=tick_on, length=duration
+                    )
+                )
 
         cevio_track_pitch_data = None
-        if unit_node.song.parameter is not None and unit_node.song.parameter.log_f0 is not None:
+        if (
+            unit_node.song.parameter is not None
+            and unit_node.song.parameter.log_f0 is not None
+        ):
             pitch_data_nodes: list[CeVIOData] = unit_node.song.parameter.log_f0.data
             pitch_datas = []
             for data_node in pitch_data_nodes:
                 if pitch_data := self.parse_pitch_data(data_node):
                     pitch_datas.append(pitch_data)
-            cevio_track_pitch_data = CeVIOTrackPitchData(events=pitch_datas, tempos=tempos, tick_prefix=tick_prefix)
+            cevio_track_pitch_data = CeVIOTrackPitchData(
+                events=pitch_datas, tempos=tempos, tick_prefix=tick_prefix
+            )
 
-        track_name = track_name or f'Track {index + 1}'
-        track = SingingTrack(title=track_name, note_list=notes)
-        if cevio_track_pitch_data is not None and (pitch := pitch_from_cevio_track(cevio_track_pitch_data)) is not None:
+        track_name = track_name or f"Track {index + 1}"
+        track = SingingTrack(
+            ai_singer_name=self.singer_id2name.get(unit_node.cast_id, ""),
+            title=track_name,
+            note_list=notes,
+            mute=group_node.is_muted,
+            solo=group_node.is_solo,
+        )
+        if (
+            cevio_track_pitch_data is not None
+            and (pitch := pitch_from_cevio_track(cevio_track_pitch_data)) is not None
+        ):
             track.edited_params.pitch = pitch
         return track, tempos, time_signatures
 
