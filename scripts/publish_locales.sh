46519ed653901007ce9d39b0c3fa4d66d8f5ec8e
SoulMelody <yjxrtzyx@gmail.com>
2023-08-04 11:44:05 +0800

    exclude translation source files when build wheels

--- scripts/publish_locales.sh
+++ scripts/publish_locales.sh
@@ -3,5 +3,5 @@ if [ -z $LIBRESVIP_LOCALE ]; then
     export LIBRESVIP_LOCALE=zh_CN
 fi
 mkdir -p ../libresvip/res/locales/"$LIBRESVIP_LOCALE"/LC_MESSAGES
-msgcat --use-first -o ../libresvip/res/locales/"$LIBRESVIP_LOCALE"/LC_MESSAGES/libresvip.po ../translations/*-"$LIBRESVIP_LOCALE".po ../libresvip/plugins/*/*-"$LIBRESVIP_LOCALE".po
-msgfmt -c -v -o ../libresvip/res/locales/"$LIBRESVIP_LOCALE"/LC_MESSAGES/libresvip.mo ../libresvip/res/locales/"$LIBRESVIP_LOCALE"/LC_MESSAGES/libresvip.po
+msgcat --use-first -o ../translations/libresvip-"$LIBRESVIP_LOCALE".po ../translations/*-"$LIBRESVIP_LOCALE".po ../libresvip/plugins/*/*-"$LIBRESVIP_LOCALE".po
+msgfmt -c -v -o ../libresvip/res/locales/"$LIBRESVIP_LOCALE"/LC_MESSAGES/libresvip.mo ../translations/libresvip-"$LIBRESVIP_LOCALE".po
