3da0cdedaf375c7058b2c6a38f28557458ff051d
SoulMelody <yjxrtzyx@gmail.com>
2024-01-18 08:27:38 +0800

    update relative pitch calculation

--- libresvip/model/relative_pitch_curve.py
+++ libresvip/model/relative_pitch_curve.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 import dataclasses
-from bisect import bisect_right
 from gettext import gettext as _
 
 from more_itertools import pairwise
 
 from libresvip.core.constants import TICKS_IN_BEAT
 from libresvip.core.exceptions import NotesOverlappedError
-from libresvip.model.base import Note, ParamCurve
+from libresvip.model.base import Note, ParamCurve, Points
 from libresvip.model.point import Point
 
 
@@ -17,49 +16,69 @@ class RelativePitchCurve:
     first_bar_length: int = TICKS_IN_BEAT * 4
 
     def to_absolute(self, points: list[Point], note_list: list[Note]) -> ParamCurve:
-        param_curve = ParamCurve()
-        boundries = [(note.start_pos, note.end_pos) for note in note_list]
-        prev_index = -1
+        return ParamCurve(
+            points=Points(root=self._convert_relativity(points, note_list, to_absolute=True))
+        )
+
+    def _convert_relativity(
+        self,
+        points: list[Point],
+        note_list: list[Note],
+        border_append_radius: int = 0,
+        to_absolute: bool = False,
+    ) -> list[Point]:
+        converted_data: list[Point] = []
+        if not len(note_list):
+            return converted_data
+        borders = self.get_borders(note_list)
+        note_index, prev_index = 0, -1
+        current_note = note_list[note_index]
+        next_border = borders[note_index] if note_index < len(borders) else float("inf")
         for point in points:
-            note_index = bisect_right(boundries, (point.x, point.x)) - 1
+            while point.x >= next_border:
+                note_index += 1
+                next_border = borders[note_index] if note_index < len(borders) else float("inf")
+                current_note = note_list[note_index]
             if note_index < 0:
                 continue
             elif note_index > prev_index:
                 prev_index = note_index
-                if len(param_curve.points) > 0:
-                    param_curve.points.append(Point(x=param_curve.points[-1].x, y=-100))
-                else:
-                    param_curve.points.append(Point.start_point())
-                param_curve.points.append(Point(x=point.x + self.first_bar_length, y=-100))
-            note = note_list[note_index]
-            if not note.start_pos <= point.x < note.end_pos:
+                if to_absolute:
+                    if len(converted_data) > 0:
+                        converted_data.append(Point(x=converted_data[-1].x, y=-100))
+                    else:
+                        converted_data.append(Point.start_point())
+                    converted_data.append(
+                        Point(
+                            x=point.x
+                            + (self.first_bar_length if to_absolute else -self.first_bar_length),
+                            y=-100,
+                        )
+                    )
+            if not current_note.start_pos <= point.x < current_note.end_pos:
                 continue
-            y = note.key_number * 100 + point.y
-            param_curve.points.append(Point(x=point.x + self.first_bar_length, y=y))
-        if len(param_curve.points) > 0:
-            param_curve.points.append(Point.end_point())
-        return param_curve
+            elif not to_absolute and point.y == -100:
+                continue
+            y = (
+                point.y
+                + (current_note.key_number if to_absolute else -current_note.key_number) * 100
+            )
+            converted_data.append(
+                Point(
+                    x=point.x + (self.first_bar_length if to_absolute else -self.first_bar_length),
+                    y=y,
+                )
+            )
+        if len(converted_data) > 0 and to_absolute:
+            converted_data.append(Point.end_point())
+        return self.append_points_at_borders(converted_data, note_list, radius=border_append_radius)
 
     def from_absolute(
         self, pitch: ParamCurve, notes: list[Note], border_append_radius: int = 0
     ) -> list[Point]:
-        if not notes:
-            return []
-        borders = self.get_borders(notes)
-        index = 0
-        current_note_key = notes[0].key_number
-        next_border = borders[index] if index < len(borders) else float("inf")
-        converted_data = []
-        for pos, value in pitch.points.root:
-            if value <= 0:
-                continue
-            while pos >= next_border:
-                index += 1
-                next_border = borders[index] if index < len(borders) else float("inf")
-                current_note_key = notes[index].key_number
-            converted_value = value - current_note_key * 100
-            converted_data.append(Point(x=pos - self.first_bar_length, y=converted_value))
-        return self.append_points_at_borders(converted_data, notes, radius=border_append_radius)
+        return self._convert_relativity(
+            pitch.points.root, notes, border_append_radius, to_absolute=False
+        )
 
     def get_borders(self, notes: list[Note]) -> list[int]:
         borders = []
