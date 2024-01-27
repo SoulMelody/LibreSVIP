79baf7a3c1968c2f809ad4efdb48fbdc3651d06f
SoulMelody <yjxrtzyx@gmail.com>
2024-01-27 09:06:15 +0800

    update wheel name

--- packaging/universal2_install.py
+++ packaging/universal2_install.py
@@ -1,5 +1,6 @@
 import contextlib
 import pathlib
+import sys
 
 import pip
 from delocate.fuse import fuse_wheels
@@ -7,6 +8,7 @@ from packaging.requirements import InvalidRequirement, Requirement
 
 if __name__ == "__main__":
     # reference: https://github.com/python-pillow/Pillow/pull/6912/
+    python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
     macos_single_platforms = ["macosx_10_10_x86_64", "macosx_11_0_arm64"]
     macos_universal_platform = "macosx_12_0_universal2"
     no_universal2_packages = [
@@ -27,6 +29,7 @@ if __name__ == "__main__":
         ) and requirement.name != "libresvip":
             if requirement.name in no_universal2_packages:
                 normalized_name = requirement.name.replace("-", "_")
+                package_version = str(requirement.specifier).lstrip("<>=")
                 for macos_platform in macos_single_platforms:
                     pip.main(
                         [
@@ -39,7 +42,7 @@ if __name__ == "__main__":
                             ":all:",
                         ]
                     )
-                universal2_wheel_name = f"{normalized_name}-universal2.whl"
+                universal2_wheel_name = f"{normalized_name}-{package_version}-{python_version}-{python_version}-{macos_universal_platform}.whl"
                 with contextlib.suppress(FileNotFoundError):
                     fuse_wheels(
                         *(str(whl_path) for whl_path in cwd.glob(f"{normalized_name}*.whl")),
@@ -47,6 +50,7 @@ if __name__ == "__main__":
                     )
                     pip.main(["install", universal2_wheel_name, "--no-deps"])
             else:
+                print(f"Installing {requirement.name} for macos_universal2")  # noqa: T201
                 pip.main(
                     [
                         "install",
