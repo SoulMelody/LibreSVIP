f648c5c370452d38e6032e448455242088ee2df0
SoulMelody <lovestar@buaa.edu.cn>
2024-03-04 10:09:25 +0800

    use pybabel compile

--- scripts/compile_catalog.sh
+++ scripts/compile_catalog.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-dir="../libresvip/res/locales/"
-ls $dir | while read line
-do
-    locale_dir=$dir$line
-    mkdir -p $locale_dir/LC_MESSAGES
-    msgfmt -c -v -o $locale_dir/LC_MESSAGES/libresvip.mo $locale_dir/LC_MESSAGES/libresvip.po
-done
+pybabel compile -d ../libresvip/res/locales/ -D libresvip
