1d0d224e21aae84ee730d76f7c47607303b74ba2
SoulMelody <yjxrtzyx@gmail.com>
2024-03-08 21:15:02 +0800

    specify dynamic library name for mingw-w64

--- libresvip/utils/audio.py
+++ libresvip/utils/audio.py
@@ -1,5 +1,6 @@
 import contextlib
 import pathlib
+import platform
 import warnings
 from typing import Optional, Union
 
@@ -16,10 +17,16 @@ def audio_track_info(
     def filter_func(track: MediaInfoTrack) -> bool:
         return track.format == "PCM" if only_wav else (track.duration is not None)
 
-    if MediaInfo.can_parse():
+    library_name = None
+    if platform.system() == "Windows":
+        python_compiler = platform.python_compiler()
+        if python_compiler.startswith("GCC"):
+            library_name = "libmediainfo-0.dll"
+
+    if MediaInfo.can_parse(library_name):
         try:
             with contextlib.suppress(ET.ParseError, RuntimeError, ValueError):
-                media_info = MediaInfo.parse(file_path)
+                media_info = MediaInfo.parse(file_path, library_name)
                 if not len(media_info.video_tracks):
                     return next(
                         (track for track in media_info.audio_tracks if filter_func(track)),
