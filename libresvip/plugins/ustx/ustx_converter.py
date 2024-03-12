6ef774c3ed1dfe5f328e28ae213dfb2c519fd917
SoulMelody <yjxrtzyx@gmail.com>
2024-03-13 04:28:57 +0800

    make ruamel.yaml as an optional dependency

--- libresvip/plugins/ustx/ustx_converter.py
+++ libresvip/plugins/ustx/ustx_converter.py
@@ -1,12 +1,9 @@
 import pathlib
 
-import yaml
-
 from libresvip.extension import base as plugin_base
 from libresvip.model.base import Project
 from libresvip.utils.text import to_unicode
-from libresvip.utils.yamlutils.dumper import get_dumper
-from libresvip.utils.yamlutils.loader import get_loader
+from libresvip.utils.yamlutils import dump_yaml_1_2, load_yaml_1_2
 
 from .model import USTXProject
 from .options import InputOptions, OutputOptions
@@ -17,17 +14,11 @@ from .ustx_parser import UstxParser
 class OpenUtauConverter(plugin_base.SVSConverterBase):
     def load(self, path: pathlib.Path, options: InputOptions) -> Project:
         proj_text = to_unicode(path.read_bytes())
-        ustx_project = USTXProject.model_validate(yaml.load(proj_text, get_loader()))
+        ustx_project = USTXProject.model_validate(load_yaml_1_2(proj_text))
         return UstxParser(options).parse_project(ustx_project)
 
     def dump(self, path: pathlib.Path, project: Project, options: OutputOptions) -> None:
         ustx_project = UstxGenerator(options).generate_project(project)
         proj_dict = ustx_project.model_dump(by_alias=True, exclude_none=True)
-        proj_text = yaml.dump(
-            proj_dict,
-            None,
-            get_dumper(grammar_version="1.2"),
-            allow_unicode=True,
-            sort_keys=False,
-        )
+        proj_text = dump_yaml_1_2(proj_dict)
         path.write_bytes(proj_text.encode("utf-8"))
