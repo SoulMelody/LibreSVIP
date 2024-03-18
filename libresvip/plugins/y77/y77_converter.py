2191b90d9c0872665d553c3fa524782a7fb30c98
SoulMelody <yjxrtzyx@gmail.com>
2024-03-18 22:05:44 +0800

    only reset tempo for projects with dynamic tempos

--- libresvip/plugins/y77/y77_converter.py
+++ libresvip/plugins/y77/y77_converter.py
@@ -17,7 +17,8 @@ class Y77Converter(plugin_base.SVSConverterBase):
         return Y77Parser(options).parse_project(y77_project)
 
     def dump(self, path: pathlib.Path, project: Project, options: OutputOptions) -> None:
-        project = reset_time_axis(project, options.tempo)
+        if len(project.song_tempo_list) != 1:
+            project = reset_time_axis(project, options.tempo)
         y77_project = Y77Generator(options).generate_project(project)
         path.write_bytes(
             json.dumps(y77_project.model_dump(mode="json", by_alias=True)).encode("utf-8")
