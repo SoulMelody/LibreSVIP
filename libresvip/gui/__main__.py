2029dde36df5831c00bc5244b3363dc9bcba3375
SoulMelody <yjxrtzyx@gmail.com>
2024-03-31 16:22:28 +0800

    gui: init locale switcher first

--- libresvip/gui/__main__.py
+++ libresvip/gui/__main__.py
@@ -4,10 +4,19 @@ import sys
 from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtQuickControls2 import QQuickStyle
 
+from libresvip.core.config import settings
 from libresvip.core.constants import res_dir
-from libresvip.gui.modules import app, app_close_event, event_loop, qml_engine
+from libresvip.gui.modules import LocaleSwitcher, app, app_close_event, event_loop, qml_engine
 
 from __feature__ import snake_case, true_property  # isort:skip # noqa: F401
+
+
+def startup() -> None:
+    qml_engine.load(":/qml/main.qml")
+    if not qml_engine.root_objects():
+        sys.exit(-1)
+    with event_loop:
+        event_loop.run_until_complete(app_close_event.wait())
 
 
 def run() -> None:
@@ -18,11 +27,9 @@ def run() -> None:
     app.application_name = "LibreSVIP"
     app.organization_name = "org.soulmelody.libresvip"
     app.window_icon = QIcon(icon_pixmap)
-    qml_engine.load(":/qml/main.qml")
-    if not qml_engine.root_objects():
-        sys.exit(-1)
-    with event_loop:
-        event_loop.run_until_complete(app_close_event.wait())
+    locale_switcher = LocaleSwitcher()
+    locale_switcher.translator_initialized.connect(startup)
+    locale_switcher.switch_language(settings.language.value)
 
 
 if __name__ == "__main__":
--- libresvip/gui/modules/locale_switcher.py
+++ libresvip/gui/modules/locale_switcher.py
@@ -1,8 +1,7 @@
 import gettext
 from typing import Optional
 
-from PySide6.QtCore import QLocale, QObject, QTranslator, Slot
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import QLocale, QObject, QTranslator, Signal, Slot
 from PySide6.QtQml import QmlElement, QmlSingleton
 
 from __feature__ import snake_case, true_property  # isort:skip # noqa: F401
@@ -11,7 +10,7 @@ from libresvip.core.config import Language, config_path, settings
 from libresvip.core.constants import PACKAGE_NAME
 from libresvip.utils import translation
 
-from .application import qml_engine
+from .application import app, qml_engine
 
 QML_IMPORT_NAME = "LibreSVIP"
 QML_IMPORT_MAJOR_VERSION = 1
@@ -40,13 +39,15 @@ class GettextTranslator(QTranslator):
 @QmlElement
 @QmlSingleton
 class LocaleSwitcher(QObject):
+    translator_initialized = Signal()
+
     def __init__(self) -> None:
         super().__init__()
+        self._translator_initialized = False
         self.translator = GettextTranslator()
         if not config_path.exists():
             sys_locale = QLocale.system().name()
             settings.language = Language.from_locale(sys_locale)
-        self.switch_language(settings.language.value)
 
     @Slot(result=str)
     def get_language(self) -> str:
@@ -56,9 +57,12 @@ class LocaleSwitcher(QObject):
     def switch_language(self, lang: str) -> None:
         if lang:
             self.translator.load_translation(lang)
-            QGuiApplication.install_translator(self.translator)
+            app.install_translator(self.translator)
             qml_engine.retranslate()
             settings.language = Language.from_locale(lang)
         else:
-            QGuiApplication.remove_translator(self.translator)
+            app.remove_translator(self.translator)
             settings.language = Language.ENGLISH
+        if not self._translator_initialized:
+            self._translator_initialized = True
+            self.translator_initialized.emit()
