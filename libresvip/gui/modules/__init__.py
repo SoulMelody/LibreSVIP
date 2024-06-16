1aba022592ddcd8ab7901dec2c583405e74d1468
SoulMelody <SoulMelody@users.noreply.github.com>
2024-06-16 12:13:37 +0800

    gui: add customized log handler

--- libresvip/gui/modules/__init__.py
+++ libresvip/gui/modules/__init__.py
@@ -14,11 +14,14 @@ __all__ = [
 import platform
 import sys
 
+from PySide6.QtCore import qInstallMessageHandler
+
 from .application import app, app_close_event, event_loop, qml_engine
 from .clipboard import Clipboard
 from .config_items import ConfigItems
 from .font_loader import IconicFontLoader
 from .locale_switcher import LocaleSwitcher
+from .log_handler import qt_log_handler
 from .task_manager import TaskManager
 
 if sys.platform == "win32":
@@ -26,6 +29,7 @@ if sys.platform == "win32":
 else:
     from libresvip.gui.modules.frameless_window import FramelessWindow
 
+qInstallMessageHandler(qt_log_handler)
 if platform.python_implementation() == "CPython":
     __all__ += ["Notifier"]
 
