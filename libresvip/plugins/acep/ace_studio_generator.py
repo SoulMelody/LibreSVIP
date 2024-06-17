198ca0e0a6d64e9f1fb7b8f1fe1e44114d038d86
SoulMelody <yjxrtzyx@gmail.com>
2024-06-18 05:15:57 +0800

    fix missing method

--- libresvip/plugins/acep/ace_studio_generator.py
+++ libresvip/plugins/acep/ace_studio_generator.py
@@ -18,11 +18,11 @@ from libresvip.model.base import (
     SongTempo,
     Track,
 )
+from libresvip.model.point import Point
 from libresvip.utils.audio import audio_track_info
 from libresvip.utils.search import binary_find_first, binary_find_last, find_last_index
 
 from .color_pool import count_color, get_color
-from .curve_segment_utils import get_value_from_segment
 from .model import (
     AcepAnchorPoint,
     AcepAnchorPoints,
@@ -440,9 +440,18 @@ class AceGenerator:
                 second += second_step
             pos = ace_curve.offset
             while pos <= curve_end and second <= right_bound:
-                ace_curve.values.append(mapping_func(get_value_from_segment(segment, tick)))
+                ace_curve.values.append(mapping_func(self.get_value_from_segment(segment, tick)))
                 pos += 1
                 tick += tick_step
                 second += second_step
             ace_curves.root.append(ace_curve)
         return ace_curves
+
+    @staticmethod
+    def get_value_from_segment(segment: list[Point], ticks: float) -> float:
+        if (left_point := binary_find_last(segment, lambda point: point.x <= ticks)) is None:
+            return segment[0].y
+        if (right_point := binary_find_first(segment, lambda point: point.x > ticks)) is None:
+            return segment[-1].y
+        ratio = (ticks - left_point.x) / (right_point.x - left_point.x)
+        return (1 - ratio) * left_point.y + ratio * right_point.y
