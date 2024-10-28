b8a8c6430ef4a01fe6dfc2ab3d7b93c1ca5a761a
SoulMelody <SoulMelody@users.noreply.github.com>
2024-10-29 06:41:06 +0800

    gui: refactor qt log handler and avoid segfault

--- libresvip/gui/modules/__init__.py
+++ libresvip/gui/modules/__init__.py
@@ -14,14 +14,12 @@ __all__ = [
 import platform
 import sys
 
-from PySide6.QtCore import qInstallMessageHandler
-
 from .application import app, app_close_event, event_loop, qml_engine
 from .clipboard import Clipboard
 from .config_items import ConfigItems
 from .font_loader import IconicFontLoader
 from .locale_switcher import LocaleSwitcher
-from .log_handler import qt_log_handler
+from .log_handler import enable_log_handler
 from .task_manager import TaskManager
 
 if sys.platform == "win32":
@@ -29,7 +27,7 @@ if sys.platform == "win32":
 else:
     from libresvip.gui.modules.frameless_window import FramelessWindow
 
-qInstallMessageHandler(qt_log_handler)
+enable_log_handler()
 if platform.python_implementation() == "CPython":
     __all__ += ["Notifier"]
 
