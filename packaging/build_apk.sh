7efcda4805e2308228dfb29110b9fd77f18fcb5d
SoulMelody <SoulMelody@users.noreply.github.com>
2024-12-16 03:41:53 +0800

    update build_apk.sh

--- packaging/build_apk.sh
+++ packaging/build_apk.sh
@@ -3,7 +3,7 @@ flet build apk \
     --android-permissions android.permission.READ_EXTERNAL_STORAGE=True android.permission.WRITE_EXTERNAL_STORAGE=True android.permission.MANAGE_EXTERNAL_STORAGE=True \
     --arch arm64-v8a \
     --include-packages flet_permission_handler \
-    --org org.soulmelody.libresvip \
+    --org org.soulmelody \
     --project LibreSVIP \
     --build-version $LIBRESVIP_VERSION \
-    --skip-flutter-doctor
+    --module-name ../libresvip/mobile/__main__.py
