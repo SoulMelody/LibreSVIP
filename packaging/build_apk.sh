828d914e242b575b9c0410b0227165188a9ae61b
SoulMelody <SoulMelody@users.noreply.github.com>
2024-12-17 04:33:27 +0800

    packging: update build_apk.sh

--- packaging/build_apk.sh
+++ packaging/build_apk.sh
@@ -1,4 +1,5 @@
 export LIBRESVIP_VERSION=`python -c 'import libresvip;print(libresvip.__version__)'`
+export FLET_VERSION=`python -c 'import flet;print(flet.version.version)'`
 flet build apk \
     --android-permissions android.permission.READ_EXTERNAL_STORAGE=True android.permission.WRITE_EXTERNAL_STORAGE=True android.permission.MANAGE_EXTERNAL_STORAGE=True \
     --arch arm64-v8a \
@@ -6,4 +7,6 @@ flet build apk \
     --org org.soulmelody \
     --project LibreSVIP \
     --build-version $LIBRESVIP_VERSION \
+    --template gh:SoulMelody/flet-build-template \
+    --template-ref $FLET_VERSION \
     --module-name ../libresvip/mobile/__main__.py
