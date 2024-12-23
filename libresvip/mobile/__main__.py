d2f6ed3bb9bebf04675749f79c298a567e000907
SoulMelody <SoulMelody@users.noreply.github.com>
2024-12-23 09:43:34 +0800

    set sys.unraisablehook

--- libresvip/mobile/__main__.py
+++ libresvip/mobile/__main__.py
@@ -1,6 +1,21 @@
+from __future__ import annotations
+
+import sys
+
 import flet as ft
+from loguru import logger
 
 from libresvip.mobile.app import main
+
+
+def unraisablehook(unraisable: sys.UnraisableHookArgs) -> None:
+    if unraisable.exc_type is not RuntimeError:
+        logger.exception(unraisable.exc_value)
+    del unraisable
+
+
+sys.unraisablehook = unraisablehook
+
 
 if __name__ == "__main__":
     ft.app(main, name="LibreSVIP")
--- libresvip/web/__main__.py
+++ libresvip/web/__main__.py
@@ -1,4 +1,19 @@
+from __future__ import annotations
+
+import sys
+
+from loguru import logger
+
 from libresvip.web.pages import main
+
+
+def unraisablehook(unraisable: sys.UnraisableHookArgs) -> None:
+    if unraisable.exc_type is not KeyboardInterrupt:
+        logger.exception(unraisable.exc_value)
+    del unraisable
+
+
+sys.unraisablehook = unraisablehook
 
 if __name__ in {"__main__", "__mp_main__"}:
     main()
