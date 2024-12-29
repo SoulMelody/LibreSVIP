16cc3d98a4d5cb12bd2ccd8bc407497f4ae3acc4
SoulMelody <SoulMelody@users.noreply.github.com>
2024-12-29 09:19:25 +0800

    remove arch argument

--- packaging/build_apk.sh
+++ packaging/build_apk.sh
@@ -2,7 +2,6 @@ export LIBRESVIP_VERSION=`python -c 'import libresvip;print(libresvip.__version_
 export FLET_VERSION=`python -c 'import flet;print(flet.version.version)'`
 flet build apk \
     --android-permissions android.permission.READ_EXTERNAL_STORAGE=True android.permission.WRITE_EXTERNAL_STORAGE=True android.permission.MANAGE_EXTERNAL_STORAGE=True \
-    --arch arm64-v8a \
     --include-packages flet_permission_handler \
     --org org.soulmelody \
     --project LibreSVIP \
