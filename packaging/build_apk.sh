8fb08aee9071db3e430f7efceb7c8929cb4ec5a1
SoulMelody <SoulMelody@users.noreply.github.com>
2025-02-16 06:41:36 +0800

    patch flet build command

--- packaging/build_apk.sh
+++ packaging/build_apk.sh
@@ -1,6 +1,6 @@
 export LIBRESVIP_VERSION=`uv run python -c 'import libresvip;print(libresvip.__version__)'`
 export FLET_VERSION=`uv run python -c 'import flet;print(flet.version.version)'`
-echo > requirements.txt
+sed -i '1441,1471d' ../.venv/lib/site-packages/flet_cli/commands/build.py
 cp  ../libresvip/mobile/__main__.py main.py
 uv run flet build apk -v \
     --android-permissions android.permission.READ_EXTERNAL_STORAGE=True android.permission.WRITE_EXTERNAL_STORAGE=True android.permission.MANAGE_EXTERNAL_STORAGE=True \
