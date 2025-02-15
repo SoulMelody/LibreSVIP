cc3a2c99cc639832ea14f2d064229332a4bf3204
SoulMelody <SoulMelody@users.noreply.github.com>
2025-02-16 06:08:02 +0800

    generate empty requirements

--- packaging/build_apk.sh
+++ packaging/build_apk.sh
@@ -1,5 +1,6 @@
 export LIBRESVIP_VERSION=`uv run python -c 'import libresvip;print(libresvip.__version__)'`
 export FLET_VERSION=`uv run python -c 'import flet;print(flet.version.version)'`
+echo > requirements.txt
 cp  ../libresvip/mobile/__main__.py main.py
 uv run flet build apk -v \
     --android-permissions android.permission.READ_EXTERNAL_STORAGE=True android.permission.WRITE_EXTERNAL_STORAGE=True android.permission.MANAGE_EXTERNAL_STORAGE=True \
