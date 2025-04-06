a6a7940fc55aa660072b15b33281f6cb64718bdb
SoulMelody <SoulMelody@users.noreply.github.com>
2025-04-07 03:29:39 +0800

    feat: add pronounciation conversion methods for hangul and cantonese

--- libresvip/middlewares/pronounciation_conversion/options.py
+++ libresvip/middlewares/pronounciation_conversion/options.py
@@ -10,7 +10,11 @@ from libresvip.utils.translation import gettext_lazy as _
 class PronounciationConversionOptions(Enum):
     NONE: Annotated[str, Field(title=_("None"))] = "Do nothing"
     HANZI2PINYIN: Annotated[str, Field(title=_("Hanzi to Pinyin"))] = "Hanzi->Pinyin"
-    TO_ROMAJI: Annotated[str, Field(title=_("To Romaji"))] = "->Romaji"
+    HANZI2JYUTPING: Annotated[str, Field(title=_("Hanzi to Jyutping"))] = "Hanzi->Jyutping"
+    HANGUL2ROMANIZATION: Annotated[str, Field(title=_("Hangul to Romanization"))] = (
+        "Hangul->Romanization"
+    )
+    KANA2ROMAJI: Annotated[str, Field(title=_("Kana to Romaji"))] = "Kana->Romaji"
     TO_HIRAGANA: Annotated[str, Field(title=_("To Hiragana"))] = "->Hiragana"
     TO_KATAKANA: Annotated[str, Field(title=_("To Katakana"))] = "->Katakana"
 
--- libresvip/middlewares/pronounciation_conversion/pronounciation_conversion.py
+++ libresvip/middlewares/pronounciation_conversion/pronounciation_conversion.py
@@ -1,3 +1,9 @@
+import functools
+import re
+
+import jyutping
+from ko_pron.ko_pron import romanise
+
 from libresvip.core.lyric_phoneme.chinese import get_pinyin_series
 from libresvip.core.lyric_phoneme.japanese import (
     to_hiragana,
@@ -14,12 +20,21 @@ from .options import ProcessOptions, PronounciationConversionOptions
 class PronounciationMiddleware(plugin_base.MiddlewareBase):
     def process(self, project: Project, options: ProcessOptions) -> Project:
         if options.mode != PronounciationConversionOptions.NONE:
-            if options.mode == PronounciationConversionOptions.TO_ROMAJI:
+            if options.mode == PronounciationConversionOptions.KANA2ROMAJI:
                 lyric_transformer = to_romaji
             elif options.mode == PronounciationConversionOptions.TO_KATAKANA:
                 lyric_transformer = to_katakana
             elif options.mode == PronounciationConversionOptions.TO_HIRAGANA:
                 lyric_transformer = to_hiragana
+            elif options.mode == PronounciationConversionOptions.HANGUL2ROMANIZATION:
+                lyric_transformer = functools.partial(romanise, system_index="rr")
+            elif options.mode == PronounciationConversionOptions.HANZI2JYUTPING:
+
+                def lyric_transformer(lyric: str) -> str:  # type: ignore[misc]
+                    return " ".join(
+                        re.sub(r"\d+$", " ", each) if each is not None else ""
+                        for each in jyutping.get(lyric)
+                    ).strip()
             else:
 
                 def lyric_transformer(lyric: str) -> str:  # type: ignore[misc]
