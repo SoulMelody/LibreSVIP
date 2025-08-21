a59ef2cf3398fcc6526b7d748814448d0abda791
SoulMelody <SoulMelody@users.noreply.github.com>
2025-08-21 17:09:00 +0800

    chore: remove useless code for pyodide

--- libresvip/utils/audio.py
+++ libresvip/utils/audio.py
@@ -49,66 +49,8 @@ if platform.system() != "Emscripten":
                 show_warning(_("Audio file not found: ") + f"{file_path}")
         return None
 else:
-    from xsdata.formats.dataclass.parsers.config import ParserConfig
-    from xsdata_pydantic.bindings import XmlParser
 
-    from .mediainfo_wasm import MediaInfo
-    from .mediainfo_wasm import Track as MediaInfoTrack
-
-    MEDIAINFO_JS_DIST_PATH = "https://fastly.jsdelivr.net/npm/mediainfo.js@latest/dist"
-
-    try:
-        import js
-        import pyodide.ffi
-
-        if not hasattr(js, "MediaInfo"):
-            js.importScripts(f"{MEDIAINFO_JS_DIST_PATH}/umd/index.min.js")
-
-        def audio_track_info(
-            file_path: str | pathlib.Path, only_wav: bool = False
-        ) -> MediaInfoTrack | None:
-            if not pyodide.ffi.can_run_sync():
-                return None
-            if isinstance(file_path, str):
-                file_path = pathlib.Path(file_path)
-
-            def filter_func(track: MediaInfoTrack) -> bool:
-                return track.format == "PCM" if only_wav else (track.duration is not None)
-
-            def read_chunk(size: int, offset: int) -> js.Uint8Array:
-                chunk = content[offset : offset + size]
-                js_buf = js.Uint8Array.new(len(chunk))
-                js_buf.assign(chunk)
-                return js_buf
-
-            def locate_file(path: str, prefix: str) -> str:
-                return f"{MEDIAINFO_JS_DIST_PATH}/MediaInfoModule.wasm"
-
-            async def parse_media_info() -> str:
-                media_info = await MediaInfo.mediaInfoFactory(format="XML", locateFile=locate_file)
-                result = await media_info.analyzeData(len(content), read_chunk)
-                media_info.close()
-                return result
-
-            if hasattr(js, "MediaInfo"):
-                if file_path.exists():
-                    with contextlib.suppress(RuntimeError, ValueError):
-                        content = file_path.read_bytes()
-                        xml_str = pyodide.ffi.run_sync(parse_media_info())
-                        media_info = XmlParser(
-                            config=ParserConfig(fail_on_unknown_properties=False)
-                        ).from_string(xml_str, MediaInfo)
-                        if not len(media_info.video_tracks):
-                            return next(
-                                (track for track in media_info.audio_tracks if filter_func(track)),
-                                None,
-                            )
-                else:
-                    show_warning(_("Audio file not found: ") + f"{file_path}")
-            return None
-    except (ImportError, SystemError):
-
-        def audio_track_info(
-            file_path: str | pathlib.Path, only_wav: bool = False
-        ) -> MediaInfoTrack | None:  # not implemented yet
-            return None
+    def audio_track_info(
+        file_path: str | pathlib.Path, only_wav: bool = False
+    ) -> MediaInfoTrack | None:  # not implemented yet
+        return None
