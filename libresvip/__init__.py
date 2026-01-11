9e05a23f7b7c6f98de57aea12f42e6f8596b8409
SoulMelody <SoulMelody@users.noreply.github.com>
2026-01-11 16:46:39 +0800

    chore: fix build error of the untokenize package when installing with python 3.14

--- libresvip/__init__.py
+++ libresvip/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 os.environ.setdefault("LOGURU_AUTOINIT", "false")
---
+++ scripts/patch_uv_lock.sh
@@ -0,0 +1,2 @@
+sed -i 's|https://pypi.tuna.tsinghua.edu.cn/packages|https://files.pythonhosted.org/packages|g' ../uv.lock
+sed -i 's|https://pypi.tuna.tsinghua.edu.cn/simple|https://pypi.org/simple|g' ../uv.lock
