ba5fadfdd085252e36e34cb682412e24d4b731af
SoulMelody <SoulMelody@users.noreply.github.com>
2026-01-20 07:39:49 +0800

    chore: remove untokenize from dependencies

--- libresvip/core/compat.py
+++ libresvip/core/compat.py
@@ -1,7 +1,5 @@
 import contextlib
-import inspect
 import sys
-from typing import Any
 
 try:
     import ujson as json
@@ -21,21 +19,3 @@ if sys.version_info < (3, 11):
     from importlib_resources.abc import Traversable
 else:
     from importlib.resources.abc import Traversable
-
-import xsdata
-from packaging.version import Version
-
-if Version(xsdata.__version__) <= Version("25.7"):
-    from xsdata.exceptions import XmlContextError
-    from xsdata.formats.dataclass.models.builders import XmlMetaBuilder
-
-    def find_declared_class(cls: XmlMetaBuilder, clazz: type, name: str) -> Any:
-        for base in clazz.__mro__:
-            ann = inspect.get_annotations(base)
-            if ann and name in ann:
-                return base
-
-        msg = f"Failed to detect the declared class for field {name}"
-        raise XmlContextError(msg)
-
-    XmlMetaBuilder.find_declared_class = classmethod(find_declared_class)
