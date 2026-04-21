81da2caf72cfd7a9153d64c9f62699ddaf366f02
SoulMelody <SoulMelody@users.noreply.github.com>
2026-04-21 14:42:48 +0800

    chore: use uv pip command

--- packaging/install_mobile_requirements.sh
+++ packaging/install_mobile_requirements.sh
@@ -1,5 +1,6 @@
 export PYTHON_PLATFORM=$1
 export WHEELS_DIR=./build/site-packages/$2
+export UV_COMPILE_BYTECODE=0
 mkdir -p $WHEELS_DIR
-uv run python -m pip install --no-deps --target $WHEELS_DIR -U --no-compile --platform $PYTHON_PLATFORM --python-version 3.12 --extra-index-url https://pypi.flet.dev/ -r requirements-android.txt
-uv run python -m pip install --no-deps --target $WHEELS_DIR -U --no-compile ../dist/*.whl
+uv pip install --no-deps --target $WHEELS_DIR -U --platform $PYTHON_PLATFORM --python-version 3.12 --extra-index-url https://pypi.flet.dev/ -r requirements-android.txt
+uv pip install --no-deps --target $WHEELS_DIR -U ../dist/*.whl
