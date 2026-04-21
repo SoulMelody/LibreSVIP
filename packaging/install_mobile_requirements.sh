2e343e74307746ba5d4ed2dc332e437595b311eb
SoulMelody <SoulMelody@users.noreply.github.com>
2026-04-21 16:46:30 +0800

    packaging: update install_mobile_requirements.sh

--- packaging/install_mobile_requirements.sh
+++ packaging/install_mobile_requirements.sh
@@ -1,6 +1,6 @@
 export PYTHON_PLATFORM=$1
 export WHEELS_DIR=./build/site-packages/$2
-export UV_COMPILE_BYTECODE=0
 mkdir -p $WHEELS_DIR
-uv pip install --no-deps --target $WHEELS_DIR -U --platform $PYTHON_PLATFORM --python-version 3.12 --extra-index-url https://pypi.flet.dev/ -r requirements-android.txt
-uv pip install --no-deps --target $WHEELS_DIR -U ../dist/*.whl
+uv run python -m ensurepip
+uv run python -m pip install --no-deps --target $WHEELS_DIR --no-compile -U --platform $PYTHON_PLATFORM --python-version 3.12 --extra-index-url https://pypi.flet.dev/ -r requirements-android.txt
+uv run python -m pip install --no-deps --target $WHEELS_DIR --no-compile -U ../dist/*.whl
