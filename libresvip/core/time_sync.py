df375cb74c4c44edb3720172a22352ffcb4a9139
SoulMelody <SoulMelody@users.noreply.github.com>
2026-05-23 23:12:11 +0800

    refactor TimeSynchronizer with bisect

--- libresvip/core/time_sync.py
+++ libresvip/core/time_sync.py
@@ -1,8 +1,8 @@
+import bisect
 from dataclasses import InitVar, dataclass, field
 
 from libresvip.core.tick_counter import skip_tempo_list
 from libresvip.model.base import SongTempo
-from libresvip.utils.search import find_last_index
 
 
 @dataclass
@@ -10,6 +10,8 @@ class TimeSynchronizer:
     is_absolute_time_code: bool = field(init=False)
     default_tempo: float = field(init=False)
     tempo_list: list[SongTempo] = field(init=False)
+    _positions: list[int] = field(init=False)
+    _cum_secs: list[float] = field(init=False)
     ori_tempo_list: InitVar[list[SongTempo]]
     skip_ticks: InitVar[int] = 0
     _is_absolute_time_code: InitVar[bool] = False
@@ -29,17 +31,37 @@ class TimeSynchronizer:
         self.is_absolute_time_code = _is_absolute_time_code
         self.default_tempo = _default_tempo
 
+        n = len(self.tempo_list)
+        self._positions = [t.position for t in self.tempo_list]
+        self._cum_secs = [0.0] * n
+        for i in range(1, n):
+            dt = self._positions[i] - self._positions[i - 1]
+            self._cum_secs[i] = self._cum_secs[i - 1] + dt / self.tempo_list[i - 1].bpm / 8
+
+    def _secs_at_tick(self, ticks: int) -> float:
+        idx = bisect.bisect_right(self._positions, ticks) - 1
+        if idx < 0:
+            idx = 0
+        return self._cum_secs[idx] + (ticks - self._positions[idx]) / self.tempo_list[idx].bpm / 8
+
+    def _ticks_at_secs(self, secs: float) -> float:
+        idx = bisect.bisect_right(self._cum_secs, secs) - 1
+        if idx < 0:
+            idx = 0
+        return self._positions[idx] + (secs - self._cum_secs[idx]) * self.tempo_list[idx].bpm * 8
+
     def get_actual_ticks_from_ticks(self, ticks: int) -> float:
         if not self.is_absolute_time_code:
             return ticks
         res = 0.0
-        i = 0
-        while i < len(self.tempo_list) - 1 and self.tempo_list[i + 1].position < ticks:
-            res += (self.tempo_list[i + 1].position - self.tempo_list[i].position) * (
+        idx = bisect.bisect_right(self._positions, ticks) - 1
+        if idx < 0:
+            idx = 0
+        for i in range(idx):
+            res += (self._positions[i + 1] - self._positions[i]) * (
                 self.default_tempo / self.tempo_list[i].bpm
             )
-            i += 1
-        res += (ticks - self.tempo_list[i].position) * self.default_tempo / self.tempo_list[i].bpm
+        res += (ticks - self._positions[idx]) * self.default_tempo / self.tempo_list[idx].bpm
         return res
 
     def get_duration_secs_from_ticks(self, start_ticks: int, end_ticks: int) -> float:
@@ -52,53 +74,15 @@ class TimeSynchronizer:
                 / self.default_tempo
                 / 8
             )
-        start_tempo_index = find_last_index(
-            self.tempo_list, lambda tempo: tempo.position <= start_ticks
-        )
-        end_tempo_index = find_last_index(
-            self.tempo_list, lambda tempo: tempo.position <= end_ticks
-        )
-        if start_tempo_index == end_tempo_index or end_tempo_index == -1:
-            return (end_ticks - start_ticks) / self.tempo_list[start_tempo_index].bpm / 8
-        secs = 0.0
-        secs += (
-            (self.tempo_list[start_tempo_index + 1].position - start_ticks)
-            / self.tempo_list[start_tempo_index].bpm
-            / 8
-        )
-        for i in range(start_tempo_index + 1, end_tempo_index):
-            secs += (
-                (self.tempo_list[i + 1].position - self.tempo_list[i].position)
-                / float(self.tempo_list[i].bpm)
-                / 8
-            )
-        secs += (
-            (end_ticks - self.tempo_list[end_tempo_index].position)
-            / float(self.tempo_list[end_tempo_index].bpm)
-            / 8
-        )
-        return secs
+        return self._secs_at_tick(end_ticks) - self._secs_at_tick(start_ticks)
 
     def get_actual_ticks_from_secs_offset(self, start_ticks: int, offset_secs: float) -> float:
         if self.is_absolute_time_code:
             return (
                 self.get_actual_ticks_from_ticks(start_ticks) + offset_secs * self.default_tempo * 8
             )
-        start_tempo_index = find_last_index(
-            self.tempo_list, lambda tempo: tempo.position <= start_ticks
-        )
-        ticks: float = start_ticks
-        secs = offset_secs
-        for i in range(start_tempo_index, len(self.tempo_list) - 1):
-            dur = (self.tempo_list[i + 1].position - ticks) / self.tempo_list[i].bpm / 8
-            if dur < secs:
-                ticks = self.tempo_list[i + 1].position
-                secs -= dur
-            else:
-                ticks += (self.tempo_list[i + 1].position - ticks) * secs / dur
-                return ticks
-        ticks += self.tempo_list[-1].bpm * secs * 8
-        return ticks
+        target_secs = self._secs_at_tick(start_ticks) + offset_secs
+        return self._ticks_at_secs(target_secs)
 
     def get_actual_ticks_from_secs(self, secs: float) -> float:
         return self.get_actual_ticks_from_secs_offset(0, secs)
