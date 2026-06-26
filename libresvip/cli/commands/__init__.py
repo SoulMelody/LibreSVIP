ae4831a916204a7aa1881d803fdccbca94092770
SoulMelody <SoulMelody@users.noreply.github.com>
2026-06-26 12:25:53 +0800

    chore: disable grpcio on win-arm64 temporally

--- libresvip/cli/commands/__init__.py
+++ libresvip/cli/commands/__init__.py
@@ -2,10 +2,15 @@ __all__ = [
     "conf_app",
     "plugin_app",
     "proj_app",
-    "rpc_app",
 ]
+
+import contextlib
 
 from .conf import app as conf_app
 from .plugin import app as plugin_app
 from .proj import app as proj_app
-from .rpc import app as rpc_app
+
+with contextlib.suppress(ImportError):
+    from .rpc import app as rpc_app
+
+    __all__ += ["rpc_app"]
